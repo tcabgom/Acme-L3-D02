2040fb8bd8cf2467da527fb7834d8627b1ca1611
Jesus Solis Ortega <jesussolisortega@gmail.com>
2023-03-02 16:51:11 +0100

    revision problems fixed

--- src/main/java/acme/entities/peep/Peep.java
+++ src/main/java/acme/entities/peep/Peep.java
@@ -2,9 +2,7 @@
 package acme.entities.peep;
 
 import java.util.Date;
-import java.util.Objects;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -45,33 +43,9 @@ public class Peep extends AbstractEntity {
 	protected String			message;
 
 	@Email
-	@NotNull
 	protected String			email;
 
 	@URL
-	@Column(nullable = true)
 	protected String			link;
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + Objects.hash(this.email, this.link, this.message, this.moment, this.nick, this.title);
-		return result;
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (this.getClass() != obj.getClass())
-			return false;
-		final Peep other = (Peep) obj;
-		return Objects.equals(this.email, other.email) && Objects.equals(this.link, other.link) && Objects.equals(this.message, other.message) && Objects.equals(this.moment, other.moment) && Objects.equals(this.nick, other.nick)
-			&& Objects.equals(this.title, other.title);
-	}
 
 }
