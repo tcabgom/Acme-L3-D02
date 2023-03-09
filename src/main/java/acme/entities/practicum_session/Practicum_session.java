374cd1bd26f98697670b7bf0d85b775864ba5012
Ignacio Gonzalez <igngongon2@alum.us.es>
2023-03-09 11:17:44 +0100

    Created practicum session class

---
+++ src/main/java/acme/entities/practicum_session/Practicum_session.java
@@ -0,0 +1,44 @@
+
+package acme.entities.practicum_session;
+
+import java.time.LocalDate;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+
+public class Practicum_session extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			abstractSession;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected LocalDate			startWeek;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected LocalDate			finishWeek;
+
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
