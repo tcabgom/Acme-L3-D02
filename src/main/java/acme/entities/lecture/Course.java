f195d41377db3f88694d258d09dcc35709b2efe5
Jesus Solis Ortega <jesussolisortega@gmail.com>
2023-03-15 19:56:30 +0100

    anotations in course added

--- src/main/java/acme/entities/lecture/Course.java
+++ src/main/java/acme/entities/lecture/Course.java
@@ -2,6 +2,7 @@
 package acme.entities.lecture;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -14,7 +15,12 @@ import org.hibernate.validator.constraints.URL;
 import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
 import acme.roles.Lecturer;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Course extends AbstractEntity {
 	// Serialisation identifier -----------------------------------------------
 
