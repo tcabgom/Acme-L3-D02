0f27663d143e1ce51f9a93dba2ef1c4f416b4dba
Jesus Solis Ortega <jesussolisortega@gmail.com>
2023-03-16 21:45:21 +0100

    all student1 sample data added

--- src/main/java/acme/entities/lecture/LecturesInCourse.java
+++ src/main/java/acme/entities/lecture/LecturesInCourse.java
@@ -1,11 +1,19 @@
 
 package acme.entities.lecture;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-public class LecturesInCourse {
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class LecturesInCourse extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
