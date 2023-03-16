2eb4c8ebe49883f76b6c52b8968218c3b74ce12c
Ignarrman <nacho.arromant@gmail.com>
2023-03-16 16:16:53 +0100

    Fixed relationships in Audit Class

--- src/main/java/acme/entities/audit/Audit.java
+++ src/main/java/acme/entities/audit/Audit.java
@@ -4,6 +4,7 @@ package acme.entities.audit;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -47,11 +48,13 @@ public class Audit extends AbstractEntity {
 	//	@NotNull
 	//	protected Mark				mark;
 
-	@ManyToOne
+	@ManyToOne(optional = false)
+	@Valid
 	@NotNull
 	protected Auditor			auditor;
 
-	@ManyToOne
+	@ManyToOne(optional = false)
+	@Valid
 	@NotNull
 	protected Course			Course;
 
